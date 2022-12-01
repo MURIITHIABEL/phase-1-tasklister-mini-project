0932a8617f10aec61d7b6efcda4bce0dcb2a3e1b
Abel Muriithi <abelmuriithi04@gmail.com>
2022-12-02 00:04:26 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  const form = document.getElementById('create_task_form');
+  const taskList = document.getElementById('tasks');
+
+
+  form.addEventListener('submit', addTask);
+
+  taskList.addEventListener('click', removeTask);
+
+  function addTask(e){
+    e.preventDefault();
+    const newTaskDescription = document.getElementById('new_task_description').value;
+    console.log(newTaskDescription);
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(newTaskDescription));
+    const deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+  }
+
+  function removeTask(e) {
+    if (e.target.nodeName === 'BUTTON') {
+      if(confirm('Delete ?')) {
+        let li = e.target.parentElement;
+        taskList.removeChild(li);
+      }
+    }
+
+  }
+
 });
